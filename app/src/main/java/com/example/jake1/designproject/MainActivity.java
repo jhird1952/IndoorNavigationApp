140d08fcc322546c586a963028c40351ef15edc1
Jesse Hird <jch170001@utdallas.edu>
2018-07-30 19:25:30 -0500

    put on github

--- app/src/main/java/com/example/jake1/designproject/MainActivity.java
+++ app/src/main/java/com/example/jake1/designproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,6 +18,22 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +56,10 @@ public class MainActivity extends AppCompatActivity {
     PopupMenu pumFrom;
     PopupMenu pumTo;
     Communicator communicator = new Communicator();
+    //URL to the web-server, change it to your web-server's URL
+    private String serverURL = "http://ecsclark18.utdallas.edu/";
+    //a variable to hold the parsed path data from ArcGIS
+    ArrayList<String[]> parsedPathData = new ArrayList<String[]>();
     double[] coordinates;
 
     @Override
@@ -180,7 +201,12 @@ public class MainActivity extends AppCompatActivity {
 
                 //once communicator is connected to server, uncomment next line and delete line after that
                 //coordinates = communicator.queryServer();
-                coordinates = new double[] {};
+                coordinates = new double[] {0, 0, 0};
+                try {
+                    getArcGISPath(etFrom.getText().toString() + ":" + etTo.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 displayPath();
 
             }
@@ -203,6 +229,17 @@ public class MainActivity extends AppCompatActivity {
                 btnStartNav.setVisibility(View.GONE);
 
                 pinchZoomPan.popCoordinates(null);
+
+            }
+        });
+
+        btnStartNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent startNavigationActivity = new Intent(getApplicationContext(), NavigationActivity.class);
+                startNavigationActivity.putExtra("finalDestination", etTo.getText().toString());
+                startActivity(startNavigationActivity);
 
             }
         });
@@ -369,6 +406,79 @@ public class MainActivity extends AppCompatActivity {
 
         pinchZoomPan.popCoordinates(coordinates);
 
+    }
+
+    //sends an HTTP Request to the PHP page, which is the URL in the StringRequest variable
+    private void getArcGISPath(final String fromToLocations) throws JSONException {
+        //send an HTTPRequest to the server to send the destination to the server
+        JSONObject test = new JSONObject();
+        test.put("from", "2.114");
+        test.put("to","2.328");
+        test.put("stairs", true);
+        test.put("elevators", true);
+        final RequestQueue httpRequestQueue = Volley.newRequestQueue(MainActivity.this);
+        final StringRequest httpSendDestination = new StringRequest(Request.Method.POST, (serverURL + "index.php"), new Response.Listener<String>() {
+
+                    @Override
+                        public void onResponse(String response) {
+                        try {
+
+                            //getting the path data and parsing it into an array
+
+                            //String[] pathData = response.split(",");
+                            //System.out.println(pathData);
+
+
+                            /*splitting the path data into individual coordinates, for every 3 commas in pathData
+                            it is considered a coordinate(z,x,y)
+                            for(int i = 0; i < pathData.length; i += 3) {
+                                String[] coordinate = new String[3];
+                                make sure we don't cause an ArrayOutOfBounds exception
+                                if(!(i + 2 >= pathData.length)) {
+                                    coordinate[0] = pathData[i];
+                                    coordinate[1] = pathData[i + 1];
+                                    coordinate[2] = pathData[i + 2];
+                                    parsedPathData.add(coordinate);
+                                }
+                            }
+
+                            output each coordinate into an array format
+                            for(int j = 0; j < parsedPathData.size(); j++) {
+                                System.out.println("[" + parsedPathData.get(j)[0] + "," + parsedPathData.get(j)[1] + "," + parsedPathData.get(j)[2] + "]");
+                            }
+                            */
+                        } catch (Exception error) {
+                            error.printStackTrace();
+                            httpRequestQueue.stop();
+                        }
+                    }
+                },
+                //error, the request responded with a failure...
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "An error occurred with the server...", Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                        httpRequestQueue.stop();
+                    }
+                }
+        ) {
+            //POST variables to send to the web-server
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("locations", fromToLocations);
+                return params;
+            }
+            //header values to send to the web-server
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        httpRequestQueue.add(httpSendDestination);
     }
 
 }
